767204265c3ecd00c14c4840cf11f4d4bd9790f7
logan <logan.g.mclachlan@gmail.com>
2023-06-05 14:16:52 +0100

    created header component

--- character-app/src/App.js
+++ character-app/src/App.js
@@ -1,9 +1,10 @@
+import Header from '../components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="App-header">Character App</h1>
+      <Header/>
     </div>
   );
 }
