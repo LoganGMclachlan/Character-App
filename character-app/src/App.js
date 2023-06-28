c954c866fae5f61a720452deae615bdef94e7175
logan <logan.g.mclachlan@gmail.com>
2023-06-28 18:57:37 +0100

    added login and register logic

--- character-app/src/App.js
+++ character-app/src/App.js
@@ -5,11 +5,12 @@ import LeftColumn from './components/LeftColumn';
 import LoginRegister from './components/LoginRegister'
 
 
-var user = {username:"Logan", password:"Password"}
-var user2 = null
-
 function App() {
   const [loginVisible, setVisible] = useState(false);
+  const userList = []
+  userList.add({username:"Logan", password:"Password"})
+  userList.add({username:"Jon", password:"Password2"})
+  var currentUser = null
   
   const renderLoginComp = () => {
     setVisible(true);
@@ -19,12 +20,26 @@ function App() {
     setVisible(false);
   }
 
-  const login = (e) => {
-    console.log("loging in")
+  const login = (username,password) => {
+    userList.forEach(e => {
+      if(e.username == username && e.password == password){
+        currentUser = e
+        alert(`Logged in as ${username}`)
+      }
+    });
   }
 
-  const register = (e) => {
-    console.log("registering")
+  const register = (username,password) => {
+    var userExists = false
+    while(!userExists){
+      userList.forEach(e => {
+        if(e.username == username){
+          userExists = true
+        }
+      });
+      currentUser = {username:username,password:password} 
+      userList.add(currentUser)
+    }
   }
 
   return (
--- character-app/src/components/LoginRegister.js
+++ character-app/src/components/LoginRegister.js
@@ -4,7 +4,66 @@ import './App.css';
 class LoginRegister extends Component {
     constructor(props){
         super(props)
+
+        this.state = {
+            lUsername:'',
+            lPassword:'',
+            rUsername:'',
+            rPassword:'',
+            rConfirmPass:''
+        }
     }
+
+    ChangeLUsername = (event) => {
+        this.setState({
+            lUsername: event.target.value
+        })
+    }
+
+    ChangeLPassword = (event) => {
+        this.setState({
+            lPassword: event.target.value
+        })
+    }
+
+    ChangeRUsername = (event) => {
+        this.setState({
+            rUsername: event.target.value
+        })
+    }
+
+    ChangeRPassword = (event) => {
+        this.setState({
+            rPassword: event.target.value
+        })
+    }
+
+    ChangeRConfirmPass = (event) => {
+        this.setState({
+            rConfirmPass: event.target.value
+        })
+    }
+
+    ChangeLUsername = (event) => {
+        this.setState({
+            lUsername: event.target.value
+        })
+    }
+
+    LoginHandler(){
+        if(state.lUsername != '' && state.lPassword != ''){
+            this.props.loginHandler(state.lUsername,state.lPassword)
+        }
+    }
+
+    RegisterHandler(){
+        if(state.rUsername != '' && state.rPassword != ''){
+            if(state.rPassword == state.rConfirmPass){
+                this.props.loginHandler(state.rUsername,state.rPassword)
+            }
+        }
+    }
+        
 
     render() { 
         return (
@@ -13,19 +72,29 @@ class LoginRegister extends Component {
             <form onSubmit={this.props.loginHandler}>
                 <h2>Login to Account</h2>
                 <label>Username:</label>
-                <input type='text'/><br/>
+                <input type='text' 
+                    value={this.state.lUsername}
+                    onChange={this.ChangeLUsername}/><br/>
                 <label>Password:</label>
-                <input type='password'/><br/>
+                <input type='password' 
+                    value={this.state.lPassword}
+                    onChange={this.Change}/><br/>
                 <button type='submit' className='red-button'>Login</button>
             </form>
             <form onSubmit={this.props.registerHandler}>
             <h2>Register new Account</h2>
                 <label>Username:</label>
-                <input type='text'/><br/>
+                <input type='text' 
+                    value={this.state.rUsername}
+                    onChange={this.Change}/><br/>
                 <label>Password:</label>
-                <input type='password'/><br/>
+                <input type='password' 
+                    value={this.state.rPassword}
+                    onChange={this.Change}/><br/>
                 <label>Confirm Password:</label>
-                <input type='password'/><br/>
+                <input type='password' 
+                    value={this.state.rConfirmPass}
+                    onChange={this.Change}/><br/>
                 <button type='submit' className='red-button'>Register</button>
             </form>
         </div>
